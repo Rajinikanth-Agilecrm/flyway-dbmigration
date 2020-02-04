1953f2e62f488f9fd2ab409425381b2da4397f92
Rajinikanth-Agilecrm <rajinikanth.jujunuri@500apps.com>
2020-02-04 16:32:48 +0530

    remove argument

--- shfiles/dbmigrations.sh
+++ shfiles/dbmigrations.sh
@@ -1,28 +1,28 @@
 echo start running
 source config.sh
-echo "enter flyway command example (migrate, clean, info, validate)"
-read commd 
+#echo "enter flyway command example (migrate, clean, info, validate)"
+#read commd 
 for ((i=1; i<=4; i++))
  do
   # echo $i
    if [ $i == 1 ];
    then 
-   flyway -user="$user1" -password="$password1" -url="$url1" $commd
+   flyway -user="$user1" -password="$password1" -url="$url1" migrate
    fi
 
    if [ $i == 2 ];
    then
-    flyway -user="$user2" -password="$password2" -url="$url2" $commd
+    flyway -user="$user2" -password="$password2" -url="$url2" migrate
    fi 
 
    if [ $i == 3 ];
    then
-    flyway -user="$user3" -password="$password3" -url="$url3" $commd
+    flyway -user="$user3" -password="$password3" -url="$url3" migrate
    fi
 
    if [ $i == 4 ];
    then
-    flyway -user="$user4" -password="$password4" -url="$url4" $commd
+    flyway -user="$user4" -password="$password4" -url="$url4" migrate
    fi
 
 done
