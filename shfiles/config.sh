2a98c65d0d75ab5c0567ca0b5078b019d525e4cf
Rajinikanth-Agilecrm <rajinikanth.jujunuri@500apps.com>
2020-02-04 19:02:36 +0530

    configure qa host

--- shfiles/config.sh
+++ shfiles/config.sh
@@ -4,9 +4,9 @@
 #url=jdbc:mysql://localhost:3306/test
 
 #1nd host
-url1=jdbc:mysql://appsdb500-1-rd.c2xawm3kje6l.us-west-2.rds.amazonaws.com:3306/test1
-user1=appsdev
-password1=Agile!234
+#url1=jdbc:mysql://appsdb500-1-rd.c2xawm3kje6l.us-west-2.rds.amazonaws.com:3306/test1
+#user1=appsdev
+#password1=Agile!234
 
 #2nd host
 url2=jdbc:mysql://appsdb-500.c2xawm3kje6l.us-west-2.rds.amazonaws.com:3306/test1
@@ -14,11 +14,11 @@ user2=appsdev
 password2=Agile!234
 
 #3rd host
-url3=jdbc:mysql://support-ly.c2xawm3kje6l.us-west-2.rds.amazonaws.com:3306/test1
-user3=appsdev
-password3=Agile!234
+#url3=jdbc:mysql://support-ly.c2xawm3kje6l.us-west-2.rds.amazonaws.com:3306/test1
+#user3=appsdev
+#password3=Agile!234
 
 #4th host
-url4=jdbc:mysql://collab-db.c2xawm3kje6l.us-west-2.rds.amazonaws.com:3306/test1
-user4=appsdev
-password4=Agile!234
+#url4=jdbc:mysql://collab-db.c2xawm3kje6l.us-west-2.rds.amazonaws.com:3306/test1
+#user4=appsdev
+#password4=Agile!234
