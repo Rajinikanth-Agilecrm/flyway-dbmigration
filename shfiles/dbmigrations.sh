8e038c4fd4e74198ef039992308c7db4170c80c1
Rajinikanth-Agilecrm <rajinikanth.jujunuri@500apps.com>
2020-02-04 15:29:09 +0530

    added new shell files

---
+++ shfiles/dbmigrations.sh
@@ -0,0 +1,28 @@
+echo start running
+source config.sh
+echo "enter flyway command example (migrate, clean, info, validate)"
+read commd 
+for ((i=1; i<=4; i++))
+ do
+  # echo $i
+   if [ $i == 1 ];
+   then 
+   flyway -user="$user1" -password="$password1" -url="$url1" $commd
+   fi
+
+   if [ $i == 2 ];
+   then
+    flyway -user="$user2" -password="$password2" -url="$url2" $commd
+   fi 
+
+   if [ $i == 3 ];
+   then
+    flyway -user="$user3" -password="$password3" -url="$url3" $commd
+   fi
+
+   if [ $i == 4 ];
+   then
+    flyway -user="$user4" -password="$password4" -url="$url4" $commd
+   fi
+
+done
