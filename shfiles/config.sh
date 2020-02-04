0f3b0635dccff2775eca4904a3c495147fbac6e2
Rajinikanth-Agilecrm <rajinikanth.jujunuri@500apps.com>
2020-02-04 18:55:32 +0530

    configure dev host

--- shfiles/config.sh
+++ shfiles/config.sh
@@ -9,16 +9,16 @@ user1=appsdev
 password1=Agile!234
 
 #2nd host
-url2=jdbc:mysql://appsdb-500.c2xawm3kje6l.us-west-2.rds.amazonaws.com:3306/test1
-user2=appsdev
-password2=Agile!234
+#url2=jdbc:mysql://appsdb-500.c2xawm3kje6l.us-west-2.rds.amazonaws.com:3306/test1
+#user2=appsdev
+#password2=Agile!234
 
 #3rd host
-url3=jdbc:mysql://support-ly.c2xawm3kje6l.us-west-2.rds.amazonaws.com:3306/test1
-user3=appsdev
-password3=Agile!234
+#url3=jdbc:mysql://support-ly.c2xawm3kje6l.us-west-2.rds.amazonaws.com:3306/test1
+#user3=appsdev
+#password3=Agile!234
 
 #4th host
-url4=jdbc:mysql://collab-db.c2xawm3kje6l.us-west-2.rds.amazonaws.com:3306/test1
-user4=appsdev
-password4=Agile!234
+#url4=jdbc:mysql://collab-db.c2xawm3kje6l.us-west-2.rds.amazonaws.com:3306/test1
+#user4=appsdev
+#password4=Agile!234
