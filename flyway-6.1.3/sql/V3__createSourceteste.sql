ad206e8246b25a14bfa75e8aefb4bdfe90a3723d
Rajinikanth-Agilecrm <rajinikanth.jujunuri@500apps.com>
2020-02-05 11:34:27 +0530

    test

---
+++ flyway-6.1.3/sql/V3__createSourceteste.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `sourceteste` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Auto-increment Id is stored',
+  `domain_id` int(10) unsigned NOT NULL COMMENT 'To store the id of particular domain.',
+  `name` varchar(50) NOT NULL COMMENT 'Name of the source is stored.',
+  `created_by` int(10) unsigned NOT NULL COMMENT 'Id of the user who created the source is stored.',
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Date on which the source is added is stored.',
+  `is_active` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'Status of source either Active(1) or Inactive(0) is stored .',
+  `updated_by` int(10) unsigned DEFAULT NULL COMMENT 'Id of the user who updated the source is stored.',
+  `updated_date` timestamp NULL DEFAULT NULL COMMENT 'Date on which the source is updated is stored.',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
