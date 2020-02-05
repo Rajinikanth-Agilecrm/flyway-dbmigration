d4b0c84dd8bd819e64cd462fe0e9733802919a59
Rajinikanth-Agilecrm <rajinikanth.jujunuri@500apps.com>
2020-02-05 17:53:25 +0530

    test 8

--- flyway-6.1.3/sql/V6_createTables6.sql
+++ flyway-6.1.3/sql/V6_createTables6.sql
@@ -1,7 +1,7 @@
-CREATE TABLE `sourcetest5` (
+CREATE TABLE `sourcetest2` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Auto-increment Id is stored',
   `domain_id` int(10) unsigned NOT NULL COMMENT 'To store the id of particular domain.',
-  `name5` varchar(50) NOT NULL COMMENT 'Name of the source is stored.',
+  `name2` varchar(50) NOT NULL COMMENT 'Name of the source is stored.',
   `created_by` int(10) unsigned NOT NULL COMMENT 'Id of the user who created the source is stored.',
   `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Date on which the source is added is stored.',
   `is_active` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'Status of source either Active(1) or Inactive(0) is stored .',
